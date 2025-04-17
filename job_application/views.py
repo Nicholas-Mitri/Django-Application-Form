22e9bf2637d2769ed781cb6900bb19d775706b01
Nicholas Mitri <nicholas.g.mitri@gmail.com>
2025-04-17 19:38:33 -0400

    Add email notification for job application submissions and configure email settings

--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+
+from mysite.settings import EMAIL_HOST_USER
 from .forms import ApplicationForm
+from .models import Form
+from django.contrib import messages
+from django.core.mail import EmailMessage
 
 
 def index(request):
@@ -12,4 +17,22 @@ def index(request):
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
 
+            Form.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                date=date,
+                occupation=occupation,
+            )
+
+            message_body = f"A new application has been submitted:\n\n"
+            message_body += f"First Name: {first_name}\nLast Name: {last_name}\nEmail: {email}\nDate: {date}\nOccupation: {occupation}"
+            email_message = EmailMessage(
+                subject="Form submission confirmation",
+                body=message_body,
+                to=[EMAIL_HOST_USER],
+            )
+            email_message.send()
+
+            messages.success(request, "Form submitted successfully!")
     return render(request, "index.html")
